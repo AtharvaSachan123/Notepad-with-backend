e72ac04d330cf96f47ecd60db7eeb1a694e839b3
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-06-28 22:19:16 +0530

    Basic Frontend

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express =require('express');
 const app = express();
+const path=require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render("index");
 });
-app.listen(3000);
+app.listen(3000, () => { 
+    console.log('Server is running on port 3000');
+});
