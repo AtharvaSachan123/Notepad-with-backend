7744d2014730672f315a1505c344faf36b97295a
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-06-28 21:45:12 +0530

    Basic setup

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const express=require('express');
-const app=express();
-const path=require('path');
+const express =require('express');
+const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')));
-app.set('view engine','ejs');
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-app.get('/',(req,res)=>{
-    res.render("index");
-})
-app.get('/profile/:username',(req,res)=>{
-    res.send(req.params.username);
-})
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000,()=>{
-    console.log("Server is running at 3000");
-})
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.listen(3000);
