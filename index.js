6da418293767859f4ba43f1f225d49d598f54ad6
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-06-28 22:39:20 +0530

    File system setup

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express =require('express');
 const app = express();
 const path=require('path');
+const fs=require('fs');
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +14,14 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-    res.render("index");
+    fs.readdir(`./files`,(err,files)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render("index",{files:files});
+        }
+    })
+    
 });
 app.listen(3000, () => { 
     console.log('Server is running on port 3000');
