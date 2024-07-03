f899a308412d9710c1d843902e744689a50add6d
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-07-03 23:31:58 +0530

    New file creation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express =require('express');
 const app = express();
 const path=require('path');
 const fs=require('fs');
+const { log } = require('console');
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -28,6 +29,14 @@ app.get('/', (req, res) => {
     })
     
 });
+
+app.post('/create',function(req,res){
+   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,(err)=>{
+        res.redirect('/');
+   })
+})
+
+
 app.listen(3000, () => { 
     console.log('Server is running on port 3000');
 });
